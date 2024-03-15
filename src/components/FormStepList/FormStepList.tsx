cbdc5171de40684ba9ef9d78bbde3fda5b54351b
RubyVine01 <leraprokofevaiera1@gmail.com>
2024-03-15 17:30:58 +0200

    add FormStepList layuot

--- src/components/FormStepList/FormStepList.tsx
+++ src/components/FormStepList/FormStepList.tsx
@@ -1,14 +1,20 @@
 import { FC } from 'react';
 import './FormStepList.scss';
+import FormStep from '../FormStep/FormStep';
 
 const FormStepList: FC = () => {
 	return (
-		<div>
-			<ul>
-				<li>Шаг 1</li>
-				<li>Шаг 2</li>
+		<section className="form-steps">
+			<h1>Создание заявки</h1>
+			<ul className="form-steps__list">
+				<FormStep title="Выбор специалиста" isFilled />
+				<FormStep title="Условия работы" isFilled />
+				<FormStep title="Обязанности и требования к&nbsp;специалисту" />
+				<FormStep title="Условия сотрудничества" />
+				<FormStep title="Требования к рекрутеру" />
+				<FormStep title="Оплата и добавление заявки" isFinalStep />
 			</ul>
-		</div>
+		</section>
 	);
 };
 
--- src/pages/EmployeeSearchFormPage/EmployeeSearchFormPage.tsx
+++ src/pages/EmployeeSearchFormPage/EmployeeSearchFormPage.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
 import './EmployeeSearchFormPage.scss';
 import { Outlet } from 'react-router-dom';
+import FormStepList from '../../components/FormStepList/FormStepList';
 
 const EmployeeSearchFormPage: FC = () => {
 	return (
-		<main>
+		<main className="employee-search-form-page">
+			<FormStepList />
 			<Outlet />
 		</main>
 	);

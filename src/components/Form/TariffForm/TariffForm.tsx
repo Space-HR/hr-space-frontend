69dd32002257fd3653045a54fd581c5c34dee7ed
RubyVine01 <leraprokofevaiera1@gmail.com>
2024-03-23 01:42:42 +0200

    add TariffRadioButton

--- src/components/Form/TariffForm/TariffForm.tsx
+++ src/components/Form/TariffForm/TariffForm.tsx
@@ -5,6 +5,7 @@ import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 import Button from '../../Buttons/Button/Button';
 import NumericInput from '../../FormElements/NumericInput/NumericInput';
+import TariffRadioButtons from '../../FormElements/TariffRadioButtons/TariffRadioButtons';
 // import { calculateMediaSalary } from '../../../utils/calculateMediaSalary';
 // import FeeRadioButtons from '../../FormElements/FeeRadioButtons/FeeRadioButtons';
 
@@ -16,45 +17,49 @@ export type TFormModel = {
 	labelForChoose: number | undefined;
 };
 
-// type TOption = {
-// 	id: number;
-// 	name: string;
-// 	value: number | undefined;
-// };
 
-// type TtariffIdOption = PropOption & {
-// 	payment_for_employee_start_working: number;
-// 	payment_for_employee_after_guarantee: number;
-// 	guarantee_period: number;
-// 	units_of_measurement_warranty_period: string;
-// };
+type TtariffIdOption =  {
+	id: number;
+	name: string;
+	payment_for_employee_start_working: number;
+	payment_for_employee_after_guarantee: number;
+	guarantee_period: number;
+	units_of_measurement_warranty_period: string;
+	description: string;
+};
 
-// const tariffIdOptions: TtariffIdOption[] = [
-// 	{
-// 		id: 0,
-// 		name: '100% за выход сотрудника',
-// 		payment_for_employee_start_working: 100,
-// 		payment_for_employee_after_guarantee: 0,
-// 		guarantee_period: 1,
-// 		units_of_measurement_warranty_period: 'month',
-// 	},
-// 	{
-// 		id: 0,
-// 		name: '50% - после выхода, 50% - после гарантийного периода',
-// 		payment_for_employee_start_working: 50,
-// 		payment_for_employee_after_guarantee: 50,
-// 		guarantee_period: 1,
-// 		units_of_measurement_warranty_period: 'month',
-// 	},
-// 	{
-// 		id: 0,
-// 		name: '100% - после гарантийного периода',
-// 		payment_for_employee_start_working: 0,
-// 		payment_for_employee_after_guarantee: 100,
-// 		guarantee_period: 1,
-// 		units_of_measurement_warranty_period: 'month',
-// 	},
-// ];
+const tariffIdOptions: TtariffIdOption[] = [
+	{
+		id: 0,
+		name: '100% за выход сотрудника',
+		payment_for_employee_start_working: 100,
+		payment_for_employee_after_guarantee: 0,
+		guarantee_period: 1,
+		units_of_measurement_warranty_period: 'month',
+		description:
+			'Отличный вариант, чтобы на заявку откликнулись «звездные» рекрутеры с опытом.',
+	},
+	{
+		id: 1,
+		name: '50% - после выхода, 50% - после гарантийного периода',
+		payment_for_employee_start_working: 50,
+		payment_for_employee_after_guarantee: 50,
+		guarantee_period: 1,
+		units_of_measurement_warranty_period: 'month',
+		description:
+			'Гарантийный период — испытательный срок. 1 месяц после выхода сотрудника.',
+	},
+	{
+		id: 2,
+		name: '100% - после гарантийного периода',
+		payment_for_employee_start_working: 0,
+		payment_for_employee_after_guarantee: 100,
+		guarantee_period: 1,
+		units_of_measurement_warranty_period: 'month',
+		description:
+			'Это 1 месяц после выхода сотрудника. Доступен выбор одного рекрутера.',
+	},
+];
 
 // const minSalary = 0;
 // const maxSalary = 200000;
@@ -130,6 +135,11 @@ const TariffForm: FC = () => {
 								placeholder="Введите вознаграждение в рублях"
 								medianSalary={medianSalary}
 							/> */}
+							<TariffRadioButtons
+								label="Модель оплаты"
+								name={fieldNames.tariffId}
+								options={tariffIdOptions}
+							/>
 						</div>
 						<div className="two-btn-disposition">
 							<Button

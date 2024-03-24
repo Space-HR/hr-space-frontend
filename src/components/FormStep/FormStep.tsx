54fa6fc265a0b29652502850b60b90b0b425b6a5
Kate Roshchupkina <katerina.ermakov@mail.ru>
2024-03-24 18:51:58 +0300

    Steps style

--- src/components/FormStep/FormStep.tsx
+++ src/components/FormStep/FormStep.tsx
@@ -2,24 +2,35 @@ import { FC } from 'react';
 import './FormStep.scss';
 
 type TFormStepsProps = {
-	// imgLink: string;
+	imgLink: string;
 	title: string;
 	isFilled?: boolean;
 	isFinalStep?: boolean;
 };
 
-const FormStep: FC<TFormStepsProps> = ({ title, isFilled, isFinalStep }) => {
+const FormStep: FC<TFormStepsProps> = ({
+	title,
+	isFilled,
+	isFinalStep,
+	imgLink,
+}) => {
 	return (
-		<li
-			className={
-				isFilled ? 'form-step form-step_active' : 'form-step form-step_inactive'
-			}
-		>
+		<li className="form-step">
 			<div className="form-step__conteiner-img-title">
-				<div className="form-step__img-conteiner" />
+				<div
+					className={`form-step__img-conteiner ${isFilled ? 'form-step__img-conteiner_active' : ''}`}
+				>
+					{imgLink && (
+						<img className="form-step__img" src={imgLink} alt={title} />
+					)}
+				</div>
 				<p className="form-step__title">{title}</p>
 			</div>
-			{isFinalStep || <div className="form-step__line" />}
+			{isFinalStep || (
+				<div
+					className={`form-step__line ${isFilled ? 'form-step__line_active' : ''}`}
+				/>
+			)}
 		</li>
 	);
 };
